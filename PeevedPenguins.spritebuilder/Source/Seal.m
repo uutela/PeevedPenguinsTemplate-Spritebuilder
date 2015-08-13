25780efa661e6161692125736b8aa8a9da71d615
Juho Uutela <uutela@gmail.com>
2015-08-13 14:07:12 +0300

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Juho Uutela on 8/13/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+@end
